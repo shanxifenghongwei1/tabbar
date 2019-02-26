5d04424a052b38e7c93351554a580c4fec96301a
liu qiang <1526950081@qq.com>
2019-02-26 10:11:22 +0800

    first

---
+++ my-project/src/pages/try/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>假的吧，这是什么逻辑啊</div>
+
+</template>
+
+<script>
+  import card from '@/components/card'
+  export default {
+    data () {
+      return{
+        msg:'吧，这是什'
+      }
+    },
+
+    components: {
+      card
+    },
+  }
+
+</script>
